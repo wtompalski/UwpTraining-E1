1858051aaab56e6badebeda103196a669fc2755d
Wojciech Tompalski <wojciech.tompalski@capgemini.com>
2020-07-15 09:47:06 +0000

    Add contacts - layout

--- UwpTraining-E1/App.xaml.cs
+++ UwpTraining-E1/App.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using UwpTraining_E1.Views;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
 using Windows.Foundation;
@@ -66,7 +67,7 @@ namespace UwpTraining_E1
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                    rootFrame.Navigate(typeof(ContactDetails), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
